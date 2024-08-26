92da510965633a75f901f0fcf69d0333f16d8705
Ryan Ali <ryanali456@gmail.com>
2024-08-26 16:42:49 +0800

    add external login in database upon first login or after registration

--- Areas/App/Controllers/ExternalAuthController.cs
+++ Areas/App/Controllers/ExternalAuthController.cs
@@ -42,30 +42,37 @@ namespace BlogApplication.Areas.App.Controllers{
                     return RedirectToAction("Index", "Login", new{Area = "App"}); 
                 }
                 var signInResult = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, 
-                info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            
+                info.ProviderKey, isPersistent: true);
+                
                 if(signInResult.Succeeded) return RedirectToAction("Index", "Dashboard", new{ Area = "App"});
+           
                 var email = info.Principal.FindFirstValue(ClaimTypes.Email);
                 var givenName = info.Principal.FindFirstValue(ClaimTypes.GivenName);
                 var surname = info.Principal.FindFirstValue(ClaimTypes.Surname);
+
                 if(string.IsNullOrEmpty(email)) return RedirectToAction("Index", "Login", new{Area = "App"}); 
 
                 var user = await _userManager.FindByEmailAsync(email);
-
                 if(user is null){
                     user = new(){
                         Email = email,
                         GivenName = givenName ?? "",
                         Surname = surname ?? "",
                         EmailConfirmed = true,
-                        UserName = email
+                        UserName = email,
                     };
                    var createResult =  await _userManager.CreateAsync(user);
                    if(!createResult.Succeeded){
                     _logger.LogError("User creation failed for user with email '{Email}' and named '{GivenName} {Surname}' ", email, givenName, surname);
                      return RedirectToAction("Index", "Login", new { Area = "App" });
                    }
-                }
+                  var createExternalLoginResult =  await _userManager.AddLoginAsync(user, info);
+                  if(!createExternalLoginResult.Succeeded){
+                     _logger.LogError("External login creation failed for user with email '{Email}' and named '{GivenName} {Surname}' ", email, givenName, surname);
+                     return RedirectToAction("Index", "Login", new { Area = "App" });
+                  }
+             }
+         
             await _signInManager.SignInAsync(user, isPersistent:true);
             return RedirectToAction("Index", "Dashboard", new {Area = "App"});
          }catch(Exception ex){
